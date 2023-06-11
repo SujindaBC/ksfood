3cd6a089b0d777a5c9048fc1f344924098970490
Sujinda Boonchuay <sujindabc@gmail.com>
2023-06-11 23:46:41 +0700

    ads slide!

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ksfood/loading/loading_screen.dart';
+import 'package:ksfood/screens/main/widgets/ads_slide.dart';
 import 'package:ksfood/screens/main/widgets/ks_drawer.dart';
 
 import '../../auth/auth_bloc/auth_bloc.dart';
@@ -18,18 +19,26 @@ class MainScreen extends StatelessWidget {
           actions: const [],
         ),
         drawer: const AppDrawer(),
-        body: Center(
-          child: Column(
-            children: [
-              const Text('Main Screen'),
-              BlocConsumer<AuthBloc, AuthState>(
-                builder: (context, state) => Text(
-                  state.user?.uid ?? "Null",
-                ),
-                listener: (context, state) {},
+        body: Column(
+          children: [
+            const AdsSlide(),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Featured Merchants",
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/screens/main/widgets/ads_slide.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+import "package:flutter_image_slideshow/flutter_image_slideshow.dart";
+
+class AdsSlide extends StatelessWidget {
+  const AdsSlide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: ImageSlideshow(
+          initialPage: 0,
+          indicatorColor: Theme.of(context).primaryColor,
+          autoPlayInterval: 7500,
+          isLoop: true,
+          children: [
+            Image.network(
+              "https://freshcart.codescandy.com/assets/images/slider/slide-1.jpg",
+              fit: BoxFit.cover,
+            ),
+            Image.network(
+              "https://freshcart.codescandy.com/assets/images/slider/slider-2.jpg",
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
